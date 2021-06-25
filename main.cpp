33d2dd23896bf3549feba9b929e1150bdd78e681
S-Schwarz <steven.schwarz@mailbox.tu-dresden.de>
2021-06-25 02:25:07 -0700

    add basic window rendering

--- main.cpp
+++ main.cpp
@@ -5,14 +5,19 @@
 #define _DEBUG
 
 #include <iostream>
+
 #include "PxPhysicsAPI.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 
 using namespace physx;
 
 static PxDefaultErrorCallback gDefaultErrorCallback;
 static PxDefaultAllocator gDefaultAllocatorCallback;
 
-int main(){
+int physxFunc(){
     PxFoundation* mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
     if (!mFoundation) {
         printf("PxCreateFoundation failed!");
@@ -58,6 +63,64 @@ int main(){
     body->release();
     mPhysics->release();
     mFoundation->release();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main(){
+
+    // Init GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Interactive Virtual Creatures", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Init GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    if(physxFunc() < 0)
+        return -1;
 
     return 0;
 }
