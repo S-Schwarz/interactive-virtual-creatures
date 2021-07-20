de4839da68a5f0bdc1f5bc437dd927327d9b1918
S-Schwarz <steven.schwarz@mailbox.tu-dresden.de>
2021-07-20 02:33:57 -0700

    include everything

---
+++ Res/glfw-build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/st/private/glfw/src/context.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/st/private/glfw/src/egl_context.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/st/private/glfw/src/glx_context.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/st/private/glfw/src/init.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/st/private/glfw/src/input.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/st/private/glfw/src/linux_joystick.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/st/private/glfw/src/monitor.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/st/private/glfw/src/osmesa_context.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/st/private/glfw/src/posix_thread.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/st/private/glfw/src/posix_time.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/st/private/glfw/src/vulkan.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/st/private/glfw/src/window.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/st/private/glfw/src/x11_init.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/st/private/glfw/src/x11_monitor.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/st/private/glfw/src/x11_window.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/st/private/glfw/src/xkb_unicode.c" "/home/st/private/glfw-build/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/st/private/glfw/include"
+  "/home/st/private/glfw/src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
