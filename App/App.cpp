fc1e127e45dcf1fe2b7d336bdade5d73c623c3e9
S-Schwarz <steven.schwarz@mailbox.tu-dresden.de>
2021-07-20 23:27:50 +0200

    add different box texture

--- App/App.cpp
+++ App/App.cpp
@@ -150,7 +150,7 @@ int ivc::App::init(){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("../Res/boxTexture.png", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../Res/boxTexture2.png", &width, &height, &nrChannels, 0);
     if(data){
         glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
